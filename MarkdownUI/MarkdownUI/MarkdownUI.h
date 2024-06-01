76a255612272d48ccef5ef7a0eeec0e88e2c81ea
Dohyeon Kim <dohyeon.kim@youversion.com>
2024-05-31 21:11:52 -0500

    Add project

---
+++ MarkdownUI/MarkdownUI/MarkdownUI.h
@@ -0,0 +1,12 @@
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MarkdownUI.
+FOUNDATION_EXPORT double MarkdownUIVersionNumber;
+
+//! Project version string for MarkdownUI.
+FOUNDATION_EXPORT const unsigned char MarkdownUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MarkdownUI/PublicHeader.h>
+
+
